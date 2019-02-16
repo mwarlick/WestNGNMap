74055d36155e77b8133b873efd9c47530a4e1605
Matthew Warlick Wilson <31708449+mwarlick@users.noreply.github.com>
2019-02-16 17:38:50 -0500

    First Commit
    Added index.html, CSS, and JS files. Folder structure for css, js, and images.

---
+++ js/map.js
@@ -0,0 +1,6 @@
+
+var map = L.map('mapid').setView([35.399, -83.26], 8.69);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/mwilson3/cjr0t37py5dh12rph5oxblmvz/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibXdpbHNvbjMiLCJhIjoiY2pyMHNrMHR5MHNmeTN5cXF4cmk0dTNpaiJ9.IrVUHrCHf8jdwV9RcYym_w', {
+}).addTo(map);
+
