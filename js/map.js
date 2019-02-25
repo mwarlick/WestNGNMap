2233a20e4a8c712be8830e8be42ff2a844ab3d2d
bsowka0 <bsowka@unca.edu>
2019-02-25 05:45:20 -0500

    no scroll on map

--- js/map.js
+++ js/map.js
@@ -1,6 +1,6 @@
 
-var map = L.map('mapid').setView([35.62493, -82.57269], 10.69);
-
+var map = L.map('mapid').setView([35.62493, -82.57269], 12.69);
+map.scrollWheelZoom.disable();
 L.tileLayer('https://api.mapbox.com/styles/v1/bsowka/cjsk5mt8s2yp01gp6jq7fkz07/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYnNvd2thIiwiYSI6ImNqcjBzaXR1NDBzYnY0YWxzaDdrajQ1NHYifQ.HTlG0daGz_x9u2dC_sMvig', {
 }).addTo(map);
 
@@ -10,7 +10,7 @@ var popup = L.popup();
 function onMapClick(e) {
     popup
         .setLatLng(e.latlng)
-        .setContent("This is what the current mbps for this area is:" )
+        .setContent("This is the current mbps for this area is:" )
         .openOn(map);
 }
 map.on('click', onMapClick);
