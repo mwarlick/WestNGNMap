074f9a9da5240f291fc2e87958268c7e3bb1ceb7
Matthew Warlick Wilson <mwarlickmedia@gmail.com>
2019-03-01 21:08:30 -0500

    Added the Map switching
    And deleted the now useless example files

--- js/map.js
+++ js/map.js
@@ -1,9 +1,46 @@
-
 var map = L.map('mapid').setView([35.62493, -82.57269], 12.69);
 map.scrollWheelZoom.disable();
-L.tileLayer('https://api.mapbox.com/styles/v1/bsowka/cjsk5mt8s2yp01gp6jq7fkz07/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYnNvd2thIiwiYSI6ImNqcjBzaXR1NDBzYnY0YWxzaDdrajQ1NHYifQ.HTlG0daGz_x9u2dC_sMvig', {
-}).addTo(map);
 
+//This Function is an if then that allows cycling between different maps.
+function mapSwitch(){
+	if (document.getElementById("map1").checked){
+			
+		//The default map goes here~!
+			L.tileLayer('https://api.mapbox.com/styles/v1/bsowka/cjsk5mt8s2yp01gp6jq7fkz07/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYnNvd2thIiwiYSI6ImNqcjBzaXR1NDBzYnY0YWxzaDdrajQ1NHYifQ.HTlG0daGz_x9u2dC_sMvig', {	
+			}).addTo(map);
+
+			console.log("map 1 is checked and loaded");
+	
+	} else if 
+		(document.getElementById("map2").checked){
+			
+			L.tileLayer('https://api.mapbox.com/styles/v1/mwilson3/cjsbs2l9c189c1ftbrzrrlk30/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibXdpbHNvbjMiLCJhIjoiY2pyMHNrMHR5MHNmeTN5cXF4cmk0dTNpaiJ9.IrVUHrCHf8jdwV9RcYym_w', {
+			}).addTo(map);
+			
+			console.log("map 2 is checked and loaded");
+			
+	} else if 
+		(document.getElementById("map3").checked){
+			
+			L.tileLayer('https://api.mapbox.com/styles/v1/mwilson3/cjsbs6amg15801go76ls8btsf/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibXdpbHNvbjMiLCJhIjoiY2pyMHNrMHR5MHNmeTN5cXF4cmk0dTNpaiJ9.IrVUHrCHf8jdwV9RcYym_w', {
+			}).addTo(map);
+			
+			console.log("map 3 is checked and loaded");
+			
+	} else if 
+		(document.getElementById("map4").checked){
+			
+			L.tileLayer('https://api.mapbox.com/styles/v1/mwilson3/cjsbs6ifr0in81gtla5dxk7vr/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibXdpbHNvbjMiLCJhIjoiY2pyMHNrMHR5MHNmeTN5cXF4cmk0dTNpaiJ9.IrVUHrCHf8jdwV9RcYym_w', {
+			}).addTo(map);
+			
+			console.log("map 4 is checked and loaded");
+			
+	} else {
+		console.error("The Map Switching Mechanic is Broken.");
+	}
+}
+
+mapSwitch;
 
 var popup = L.popup();
 
